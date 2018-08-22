c7042f1883e105bb156f6a854d29407bca20cf29
anthooooooooony <reallovexy@gmail.com>
2018-08-23 00:29:31 +1000

    Update

--- autoencoder.py
+++ autoencoder.py
@@ -1,18 +1,36 @@
-import pandas as pd
+import sys
+import numpy as np
+from scipy.sparse import coo_matrix, vstack
+
+
 
 TRAIN_FILE = 'train.txt'
 
 axis_dict = {}
 
 with open(TRAIN_FILE, 'r') as f:
-    index_num = 0
-    for i in range(2):
-        line = f.readline()
+    for line in f.readlines():
         line = line.strip()
         line = line.split('\t')
-        for index, item in enumerate(line):
-            axis_dict[item] = i + index
-        index_num += len(line)
+        # print len(line)
+        for uid in line:
+            axis_dict.setdefault(uid, len(axis_dict))
 
 
-#def matrixConstruct():
+"""
+with open(TRAIN_FILE, 'r') as f:
+    for line in f.readlines():
+        line = line.strip()
+        line = line.split('\t')
+        row = np.ones((1, len(line))) # row index
+        l = [axis_dict[tar_node] for tar_node in line]
+        col = np.array(l) # col index
+        data = np.ones(1, len(line))
+        data[1, axis_dict[line[0]]] = 0
+        matrix(data, (row, col))
+
+print matrix.toarray()[:2, :167]
+"""
+
+
+
