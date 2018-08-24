59201962a2b1eb181d960c58e9e2d5e053cbcb9a
anthooooooooony <reallovexy@gmail.com>
2018-08-24 22:37:28 +1000

    update

--- autoencoder.py
+++ autoencoder.py
@@ -3,34 +3,19 @@ import numpy as np
 from scipy.sparse import coo_matrix, vstack
 
 
-
 TRAIN_FILE = 'train.txt'
 
-axis_dict = {}
+data = {}
 
 with open(TRAIN_FILE, 'r') as f:
     for line in f.readlines():
         line = line.strip()
         line = line.split('\t')
-        # print len(line)
-        for uid in line:
-            axis_dict.setdefault(uid, len(axis_dict))
+        data.setdefault(line[0], line[1:])
 
+print 'The length of the dict is {}'.format(len(data))
 
-"""
-with open(TRAIN_FILE, 'r') as f:
-    for line in f.readlines():
-        line = line.strip()
-        line = line.split('\t')
-        row = np.ones((1, len(line))) # row index
-        l = [axis_dict[tar_node] for tar_node in line]
-        col = np.array(l) # col index
-        data = np.ones(1, len(line))
-        data[1, axis_dict[line[0]]] = 0
-        matrix(data, (row, col))
-
-print matrix.toarray()[:2, :167]
-"""
-
+for i in sorted(data.keys()[:10]):
+        print 'The key is {}, the value is {}'.format(i, data[i])
 
 
