88171f1a2e43caf35e01420307fd30b69d48601e
anthooooooooony <reallovexy@gmail.com>
2018-08-26 00:54:10 +1000

    Update

--- autoencoder.py
+++ autoencoder.py
@@ -1,6 +1,8 @@
-import sys
-import numpy as np
+import time
+import pickle
 from scipy.sparse import coo_matrix, vstack
+import networkx as nx
+
 
 
 TRAIN_FILE = 'train.txt'
@@ -8,14 +10,34 @@ TRAIN_FILE = 'train.txt'
 data = {}
 
 with open(TRAIN_FILE, 'r') as f:
-    for line in f.readlines():
+    for line in f.readlines()[:20]:
         line = line.strip()
         line = line.split('\t')
         data.setdefault(line[0], line[1:])
 
 print 'The length of the dict is {}'.format(len(data))
+print
+"""
+for i in sorted(data.keys()):
+        print 'The key is {}, the value is {}'.format(i, len(data[i]))
+        print
+"""
+start_time = time.time()
 
-for i in sorted(data.keys()[:10]):
-        print 'The key is {}, the value is {}'.format(i, data[i])
+graph = nx.DiGraph(data)
+print 'Graph constructing time: {}'.format(time.time() - start_time)
+print 'The number of nodes: {}'.format(graph.number_of_nodes())
+print
 
+start_time = time.time()
 
+adjm = nx.adjacency_matrix(graph)
+print 'Adjacency constructing time: {}'.format(time.time() - start_time)
+
+binf = open('adjm.bin', 'wb')
+start_time = time.time()
+pickle.dump(adjm, binf)
+print 'pickle storing time: {}'.format(time.time() - start_time)
+binf.close()
+
+#print adjm.todense()
